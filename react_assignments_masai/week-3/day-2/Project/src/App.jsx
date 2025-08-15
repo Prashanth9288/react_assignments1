bdc3526597fd7c1ab17f5f8d8fe7a4d3928cddeb
uppara prashanth <prashanth939288@gmail.com>
2025-08-15 23:09:47 +0530

    done

--- react_assignments_masai/week-3/day-2/Project/src/App.jsx
+++ react_assignments_masai/week-3/day-2/Project/src/App.jsx
@@ -1,29 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
-import { ProjectsProvider } from './context/ProjectsContext.jsx';
-import PrivateRoute from './routes/PrivateRoute.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import ProjectDetails from './pages/ProjectDetails.jsx';
-import AddProject from './pages/AddProject.jsx';
-import EditProject from './pages/EditProject.jsx';
-import Login from './pages/Login.jsx';
-import Signup from './pages/Signup.jsx';
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <ProjectsProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/projects/new" element={<PrivateRoute><AddProject /></PrivateRoute>} />
-            <Route path="/projects/:id" element={<PrivateRoute><ProjectDetails /></PrivateRoute>} />
-            <Route path="/projects/:id/edit" element={<PrivateRoute><EditProject /></PrivateRoute>} />
-          </Routes>
-        </BrowserRouter>
-      </ProjectsProvider>
-    </AuthProvider>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
